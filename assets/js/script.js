c8b69454aa074182fb982a5e22a2ad683c00501b
Takaforyannick30 <takaforyannick30@gmail.com>
2020-07-22 01:24:08 +0000

    Create sounds folder and declare sound variables

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------Sound Variables
+const bgSound = new Audio("assets/sounds/background-sound.mp3");
+const flipSound = new Audio("assets/sounds/card-flip-sound.wav");
+const matchSound = new Audio("assets/sounds/card-match-sound.wav");
+const gameCompleteSound = new Audio("assets/sounds/game-complete-sound.wav");
+const gameOverSound = new Audio("assets/sounds/game-over-sound.wav");
+const gameVictorySound = new Audio("assets/sounds/game-victory-sound.wav");
+
+
