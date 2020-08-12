77cd4ecbdc30c33b0a3e57c8ecd3053189566e65
Takaforyannick30 <takaforyannick30@gmail.com>
2020-08-12 00:15:33 +0000

    Do script.js validation, delete 21 unused variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,22 @@
 const bgSound = new Audio("assets/sounds/background-sound.mp3");
 const flipSound = new Audio("assets/sounds/card-flip-sound.wav");
 const matchSound = new Audio("assets/sounds/card-match-sound.wav");
-const gameCompleteSound = new Audio("assets/sounds/game-complete-sound.wav");
+
 const gameOverSound = new Audio("assets/sounds/game-over-sound.wav");
 const gameVictorySound = new Audio("assets/sounds/game-victory-sound.wav");
-
-//------------------------------------------- Game Button Variables
-
-const startGameButton = $("new-game-button");
-const playNextLevelButton = $("#play-next-level-button");
-const volumeControl = $(".sound-control");
-const closeGameInfoButton = $(".close-game-info-button");
 
 // ------------------------------------------ Variables
 
 let flags = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ];
 
-
-let gameRecord = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; //keep track of flip cards
-let imageRecord = []; //keep track of randomly loaded and yet to be loaded images
 let flipIndex = 0; //keep track of the number of flipped cards
 let firstFlippedCard;//keep track of the first flipped card
 let secondFlippedCard;// keep track of the second flipped card 
 let previousCard; // keep track of the previous flipped card
-let randomNumber; //will be used to randomly generate numbers
-let cardTextRecord = []; // store the string of images and check for comparison
-let cardRecord = []; //store the id of every 2 flipped cards
-let cardNumber = []; //extract the number of the particular id of flipped cards
-let front; //dynamically contain the front element of the card
-let back; //dynamically contain the back element of card
-let cardCheck = 0; //Restrict only two cards to be flipped and checked at a time
 let score = 0; //number of matches made
-let status = 0; //keep track of the randomNumber's uniqueness
 let countDown; //keep track of countdown timer
 let secondsInput = 60; //keep track of timer from 60seconds
 let seconds = secondsInput; 
-let gameLevel = 1; // start game from level 1
-let currentLevel = gameLevel; //will increment as game level changes
 let gameOver = false; 
 
 // ------------------------------- Variables retrieve from HTML elements
@@ -46,11 +26,6 @@ let gameOver = false;
 let gameArea = $("#deck-of-cards-container");
 let time = $(".time-left");
 let scoreRecord = $(".matches-made");
-let flipsRecord = $(".flips-made");
-let levelRecord = $(".current-level");
-let victoryResult = $("#result-for-success");
-let finalVictoryResult = $("result-for final success");
-let faillureResult = $("#result-for-faillure");
 
 // Function to prevent intro-overlay-image from being dragged
 $("#intro-overlay-image").attr('draggable', false);
